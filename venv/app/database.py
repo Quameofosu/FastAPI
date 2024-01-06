44d302efe6b90af7574ea5c45235279e5b6fa9a4
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-06 17:54:17 +0000

    initial commit

--- venv/app/database.py
+++ venv/app/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+# import psycopg2
+# from psycopg2.extras import RealDictCursor
+# import time
 
 SQLALCHEMY_DATABASE_URL = "postgresql://postgres:xJOYce$%^@localhost/fastapi"
 #SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
@@ -19,3 +22,17 @@ def get_db():
         yield db
     finally:
         db.close()
+
+
+# while True:
+#     try:
+#     # Connect to an existing database
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres',
+#                              password='xJOYce$%^', cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successfull!")
+#         break
+#     except Exception as error:
+#         print("Connecting to database failed")
+#         print("Error:", error)
+#         time.sleep(2)
