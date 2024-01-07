75cce2cc9913e47ec1e825aa02a2d123ee2a171a
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-07 11:00:12 +0000

    initial commit

--- app/oauth2.py
+++ app/oauth2.py
@@ -4,15 +4,17 @@ from . import schemas, database, models
 from sqlalchemy.orm import Session
 from fastapi import Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordBearer
+from .config import settings
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 # SECRET_KEY
 # ALGORITHM
 # ACCESS_TOKEN_EXPIRE_MINUTES
 
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
+
 
 def create_access_token(data: dict):
     to_encode = data.copy()
@@ -24,23 +26,29 @@ def create_access_token(data: dict):
 
     return encoded_jwt
 
+
 def verify_access_token(token: str, credentials_exception):
-    
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         id: str = payload.get("user_id")
 
         if id is None:
             raise credentials_exception
-        token_data = schemas.TokenData(id= str(id))
+        token_data = schemas.TokenData(id=str(id))
     except JWTError:
         raise credentials_exception
-    
+
     return token_data
 
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
-    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail=f"Could not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=f"Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
     token = verify_access_token(token, credentials_exception)
     user = db.query(models.User).filter(models.User.id == token.id).first()
 
