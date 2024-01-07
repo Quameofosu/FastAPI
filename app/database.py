711bf3b81173ffc79aab4ad07d56bbc1c11c4f61
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-07 10:33:05 +0000

    initial commit

--- app/database.py
+++ app/database.py
@@ -1,19 +1,21 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from .config import settings
+
 # import psycopg2
 # from psycopg2.extras import RealDictCursor
 # import time
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:xJOYce$%^@localhost/fastapi"
-#SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
 
 # Dependency
 def get_db():
