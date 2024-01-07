6bf69b646ca38dfd7cdcf5baa017a9ab932d0f0d
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-07 09:18:23 +0000

    Moved app folder one step higher

--- app/main.py
+++ app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from .routers import post, user, auth
 from . import models
 from .database import engine
+from .config import settings
 
 models.Base.metadata.create_all(bind=engine)
 
--- app/routers/user.py
+++ app/routers/user.py
@@ -5,11 +5,11 @@ from .. import models, schemas, utils
 from ..database import get_db
 
 
-router = APIRouter(prefix="/users",tags=["User"])
+router = APIRouter(prefix="/users", tags=["User"])
 
 
-#-----------------------------------------------------------------------------------------------
-@router.post("/", status_code= status.HTTP_201_CREATED, response_model=schemas.UserOut)
+# -----------------------------------------------------------------------------------------------
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     # Hash the password - user.password
     hashed_password = utils.hash(user.password)
@@ -20,10 +20,13 @@ def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db.refresh(new_user)
     return new_user
 
+
 @router.get("/{id}", response_model=schemas.UserOut)
 def get_user(id: int, db: Session = Depends(get_db)):
     user = db.query(models.User).filter(models.User.id == id).first()
     if not user:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND,
-                            detail = f"user with id: {id} was not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"user with id: {id} was not found",
+        )
     return user
