741a929c0a2321c31737bd056fc5c50fbe55cd09
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-08 22:27:58 +0000

    added Vote class

--- app/models.py
+++ app/models.py
@@ -5,7 +5,8 @@ from sqlalchemy.sql.expression import text
 
 from .database import Base
 
-# All the classes are for holding the Tables 
+# All the classes are for holding the Tables
+
 
 class Post(Base):
     __tablename__ = "posts"
@@ -13,24 +14,34 @@ class Post(Base):
     id = Column(Integer, primary_key=True, nullable=False)
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean, server_default='True', nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), 
-                        nullable=False, server_default=text('now()'))
-    owner_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+    owner_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False
+    )
 
     owner = relationship("User")
 
 
-
-
-
 # Handle User registration
 class User(Base):
-    __tablename__ = 'users'
+    __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, nullable=False)
     email = Column(String, nullable=False, unique=True)
     password = Column(String, nullable=False)
-    created_at = Column(TIMESTAMP(timezone=True), 
-                        nullable=False, server_default=text('now()'))
-    
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
+
+
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
+    post_id = Column(
+        Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True
+    )
