aab22c67394291902fac554f32ef3a82c0ff5f6b
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-15 05:27:19 +0000

    initial commit

--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from .routers import post, user, auth, vote
 from . import models
 from .database import engine
@@ -7,6 +9,22 @@ from .config import settings
 # models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # []
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(post.router)
 app.include_router(user.router)
