b197f290ccae228f23784a4b412725803338d653
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-15 02:28:16 +0000

    initial commit

---
+++ alembic/versions/5e84251ec93a_create_post_table.py
@@ -0,0 +1,26 @@
+"""create post table
+
+Revision ID: 5e84251ec93a
+Revises: 
+Create Date: 2024-01-15 02:25:05.842500
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '5e84251ec93a'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
