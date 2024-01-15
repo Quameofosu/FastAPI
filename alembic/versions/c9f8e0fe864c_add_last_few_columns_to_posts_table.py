b0d831d4267b5866907eaa531fe84f8f95a992cb
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-15 05:06:04 +0000

    initial commit

--- alembic/versions/c9f8e0fe864c_add_last_few_columns_to_posts_table.py
+++ alembic/versions/c9f8e0fe864c_add_last_few_columns_to_posts_table.py
@@ -12,15 +12,30 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = 'c9f8e0fe864c'
-down_revision: Union[str, None] = 'f8863da1e658'
+revision: str = "c9f8e0fe864c"
+down_revision: Union[str, None] = "f8863da1e658"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
 
 def upgrade() -> None:
+    op.add_column(
+        "posts",
+        sa.Column("published", sa.Boolean(), nullable=False, server_default="TRUE"),
+    )
+    op.add_column(
+        "posts",
+        sa.Column(
+            "created_at",
+            sa.TIMESTAMP(timezone=True),
+            nullable=False,
+            server_default=sa.text("NOW()"),
+        ),
+    )
     pass
 
 
 def downgrade() -> None:
+    op.drop_column("posts", "published")
+    op.drop_column("posts", "created_at")
     pass
