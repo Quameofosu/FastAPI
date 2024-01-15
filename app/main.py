34941089828274ce93ea6c01e82a7e6adf3ccb81
Manuel Ofosu <quameofosuemma@gmail.com>
2024-01-15 05:19:55 +0000

    initial commit

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ from . import models
 from .database import engine
 from .config import settings
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
